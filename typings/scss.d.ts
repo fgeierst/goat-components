7c18d739c48b96bdfab5a2ac56c13b24da11b898
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-04-06 21:06:55 +0200

    Feat: translate variables

--- typings/scss.d.ts
+++ typings/scss.d.ts
@@ -1,11 +1,5 @@
 declare module '*.scss?inline' {
-  import { CSSResult} from 'lit';
-  const styles: CSSResult;
-  export default styles;
-}
-
-declare module '*.scss' {
-  import { CSSResult} from 'lit';
-  const styles: CSSResult;
-  export default styles;
+	import { CSSResult } from 'lit';
+	const styles: CSSResult;
+	export default styles;
 }
