66c29f41be79ca66bea1207aef48b058821b9c43
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-04-13 22:55:35 +0200

    feat: searchable menu component

--- dist/goat-components.js
+++ dist/goat-components.js
@@ -1,27 +1,27 @@
-import { unsafeCSS as d, LitElement as u, html as b } from "lit";
-import { property as p, customElement as h } from "lit/decorators.js";
-import { classMap as c } from "lit/directives/class-map.js";
+import { unsafeCSS as f, LitElement as m, html as i } from "lit";
+import { property as p, customElement as _, state as v } from "lit/decorators.js";
+import { classMap as b } from "lit/directives/class-map.js";
 const x = {
   prefix: "goat"
-}, _ = `.hamburger{all:unset;position:relative;background-color:var(--_color-background);width:48px;height:48px}.hamburger:before,.hamburger:after{content:"";display:block;width:24px;height:3px;position:absolute;left:12px;background-color:var(--_color-text);transition:transform .2s}.hamburger:before{top:18px}.hamburger[aria-expanded=true]:before{top:calc(50% - 1.5px);transform:rotate(-45deg)}.hamburger:after{bottom:18px}.hamburger[aria-expanded=true]:after{top:calc(50% - 1.5px);transform:rotate(45deg)}:host{--_transition-duration: var(--transition-duration, .2s);--_color-text: var(--color-text, black);--_color-background: var(--color-background, white);--_translateX: var(--translateX, 0);--_translateY: var(--translateY, -100%);position:relative}.clipbox{position:absolute;display:grid;overflow:hidden;transition:visibility var(--_transition-duration) linear}[aria-expanded=false]+.clipbox{visibility:hidden}.clipbox__panel{transition:transform var(--_transition-duration) cubic-bezier(.77,0,.175,1);background-color:var(--_color-background)}[aria-expanded=false]+.clipbox>.clipbox__panel{transform:translate(var(--_translateX),var(--_translateY))}.visually-hidden{position:absolute;transform:scale(0)}:focus-visible{outline:2px solid;outline-offset:2px}
+}, g = `.hamburger{all:unset;position:relative;background-color:var(--_color-background);width:48px;height:48px}.hamburger:before,.hamburger:after{content:"";display:block;width:24px;height:3px;position:absolute;left:12px;background-color:var(--_color-text);transition:transform .2s}.hamburger:before{top:18px}.hamburger[aria-expanded=true]:before{top:calc(50% - 1.5px);transform:rotate(-45deg)}.hamburger:after{bottom:18px}.hamburger[aria-expanded=true]:after{top:calc(50% - 1.5px);transform:rotate(45deg)}:host{--_transition-duration: var(--transition-duration, .2s);--_color-text: var(--color-text, black);--_color-background: var(--color-background, white);--_translateX: var(--translateX, 0);--_translateY: var(--translateY, -100%);position:relative}.clipbox{position:absolute;display:grid;overflow:hidden;transition:visibility var(--_transition-duration) linear}[aria-expanded=false]+.clipbox{visibility:hidden}.clipbox__panel{transition:transform var(--_transition-duration) cubic-bezier(.77,0,.175,1);background-color:var(--_color-background)}[aria-expanded=false]+.clipbox>.clipbox__panel{transform:translate(var(--_translateX),var(--_translateY))}.visually-hidden{position:absolute;transform:scale(0)}:focus-visible{outline:2px solid;outline-offset:2px}
 `;
-var f = Object.defineProperty, v = Object.getOwnPropertyDescriptor, n = (o, r, i, a) => {
-  for (var e = a > 1 ? void 0 : a ? v(r, i) : r, s = o.length - 1, l; s >= 0; s--)
-    (l = o[s]) && (e = (a ? l(r, i, e) : l(e)) || e);
-  return a && e && f(r, i, e), e;
+var y = Object.defineProperty, S = Object.getOwnPropertyDescriptor, c = (e, t, n, a) => {
+  for (var r = a > 1 ? void 0 : a ? S(t, n) : t, s = e.length - 1, o; s >= 0; s--)
+    (o = e[s]) && (r = (a ? o(t, n, r) : o(r)) || r);
+  return a && r && y(t, n, r), r;
 };
-let t = class extends u {
+let l = class extends m {
   constructor() {
     super(), this.buttonLabel = "Menu", this.expanded = !1, this.hamburgerIcon = !1, this._onEscapeKeyBound = null;
   }
   render() {
-    return b`
+    return i`
 			<button
-				class=${c({ hamburger: this.hamburgerIcon })}
+				class=${b({ hamburger: this.hamburgerIcon })}
 				aria-expanded=${this.expanded ? "true" : "false"} 
 				@click=${this._onClick} part="button"
 			>
-				<span class=${c({ "visually-hidden": this.hamburgerIcon })}>
+				<span class=${b({ "visually-hidden": this.hamburgerIcon })}>
 					${this.buttonLabel}
 				</span>
 			</button>
@@ -42,23 +42,150 @@ let t = class extends u {
   _onClick() {
     this.expanded ? this._close() : this._open();
   }
-  _onEscapeKey(o) {
-    o.key === "Escape" && this._close();
+  _onEscapeKey(e) {
+    e.key === "Escape" && this._close();
   }
 };
-t.styles = d(_);
-n([
+l.styles = f(g);
+c([
   p({ type: String })
-], t.prototype, "buttonLabel", 2);
-n([
+], l.prototype, "buttonLabel", 2);
+c([
   p({ type: Boolean, reflect: !0 })
-], t.prototype, "expanded", 2);
-n([
+], l.prototype, "expanded", 2);
+c([
   p({ type: Boolean })
-], t.prototype, "hamburgerIcon", 2);
-t = n([
-  h(`${x.prefix}-slideout`)
-], t);
+], l.prototype, "hamburgerIcon", 2);
+l = c([
+  _(`${x.prefix}-slideout`)
+], l);
+const $ = `.panel{border:2px solid}
+`;
+var I = Object.defineProperty, w = Object.getOwnPropertyDescriptor, h = (e, t, n, a) => {
+  for (var r = a > 1 ? void 0 : a ? w(t, n) : t, s = e.length - 1, o; s >= 0; s--)
+    (o = e[s]) && (r = (a ? o(t, n, r) : o(r)) || r);
+  return a && r && I(t, n, r), r;
+};
+let u = class extends m {
+  constructor() {
+    super(), this.menu = null, this._currentSubmenuId = 0, this._searchprompt = "";
+  }
+  render() {
+    return i`
+			<form>
+				<input type="search" placeholder="Search..."
+					@input=${(e) => this._searchprompt = e.target.value} 
+
+				/>
+				<button type="submit">Search</button>
+			</form>
+
+			<div class="panel">
+				${this._searchprompt ? this.renderSearchResults() : this.renderSubmenu(this.getSubmenuById(this._currentSubmenuId))}
+			</div>
+		`;
+  }
+  renderSubmenu(e) {
+    return i`
+			<h2>${e.label}</h2>
+			<ul>
+				${e.items.map((t) => this.renderItem(t))}
+			</ul>
+		`;
+  }
+  getSubmenuById(e) {
+    return this.getSubmenus(this.menu).find((t) => t.id === e);
+  }
+  getItems(e) {
+    const t = [];
+    function n(a) {
+      const { id: r, label: s, href: o, items: d } = a;
+      t.push({
+        id: r,
+        label: s,
+        href: o
+      }), d && d.forEach(n);
+    }
+    return n(e), t;
+  }
+  getSubmenus(e) {
+    const t = [];
+    function n(a) {
+      const r = [];
+      a.items && a.items.forEach((s) => {
+        r.push({
+          id: s.id,
+          label: s.label,
+          href: s.href,
+          hasSubmenu: !!s.items
+        }), s.items && n({
+          id: s.id,
+          label: s.label,
+          items: s.items
+        });
+      }), t.push({
+        id: a.id,
+        label: a.label,
+        items: r
+      });
+    }
+    return n(e), t;
+  }
+  renderSearchResults() {
+    console.log(this.getItems(this.menu));
+    const e = this.getItems(this.menu).filter((t) => t.label.toLowerCase().startsWith(this._searchprompt.toLowerCase()));
+    return i`
+			<h2>Search Resuls</h2>
+			<ul>
+				${e.length === 0 ? i` 
+					<li> No results found. </li>
+				` : e.map((t) => i`
+						<li>
+							<a href="${t.href}">
+								${t.label}
+							</a>
+						</li>
+					`)}
+			</ul>
+		`;
+  }
+  renderItem(e) {
+    return e.hasSubmenu ? i`
+				<li>
+					<button 
+						@click=${() => this.switchTo(e.id)}
+					>
+						${e.label}
+					</button>
+				</li>
+			` : i`
+			<li>
+				<a href="${e.href}">
+					${e.label}
+				</a>
+			</li>
+		`;
+  }
+  switchTo(e) {
+    this._currentSubmenuId = e;
+  }
+};
+u.styles = f($);
+h([
+  p({
+    attribute: !1
+  })
+], u.prototype, "menu", 2);
+h([
+  v()
+], u.prototype, "_currentSubmenuId", 2);
+h([
+  v()
+], u.prototype, "_searchprompt", 2);
+u = h([
+  _(`${x.prefix}-searchable-menu`)
+], u);
 export {
-  t as Slideout
+  u as SearchableMenu,
+  l as Slideout
 };
---
+++ src/components/searchable-menu/searchable-menu.ts
@@ -0,0 +1,151 @@
+import { LitElement, unsafeCSS, html } from 'lit'
+import { customElement, property, state } from 'lit/decorators.js';
+
+import { settings } from '../settings'
+import style from './searchable-menu.scss?inline';
+
+@customElement(`${settings.prefix}-searchable-menu`)
+export class SearchableMenu extends LitElement {
+
+	@property({
+		attribute: false,
+	}) menu = null;
+
+	@state() _currentSubmenuId: number = 0;
+	@state() _searchprompt: string = '';
+
+	constructor() {
+		super();
+	}
+
+	static styles = unsafeCSS(style);
+
+	render() {
+		return html`
+			<form>
+				<input type="search" placeholder="Search..."
+					@input=${e => this._searchprompt = e.target.value} 
+
+				/>
+				<button type="submit">Search</button>
+			</form>
+
+			<div class="panel">
+				${this._searchprompt
+				? this.renderSearchResults()
+				: this.renderSubmenu(this.getSubmenuById(this._currentSubmenuId))
+			}
+			</div>
+		`;
+	}
+
+	renderSubmenu(item) {
+		return html`
+			<h2>${item.label}</h2>
+			<ul>
+				${item.items.map(item => this.renderItem(item))}
+			</ul>
+		`;
+	}
+
+	getSubmenuById(id) {
+		return this.getSubmenus(this.menu).find(item => item.id === id);
+	}
+	getItems(menu) {
+		const array = [];
+		function addItem(item) {
+			const { id, label, href, items } = item;
+			array.push({
+				id,
+				label,
+				href
+			});
+			if (items) {
+				items.forEach(addItem);
+			}
+		}
+		addItem(menu);
+		return array;
+	}
+
+	getSubmenus(menu) {
+		const array = [];
+		function add(obj) {
+			const items = [];
+			if (obj.items) {
+				obj.items.forEach(item => {
+					items.push({
+						id: item.id,
+						label: item.label,
+						href: item.href,
+						hasSubmenu: item.items ? true : false,
+					})
+					if (item.items) {
+						add({
+							id: item.id,
+							label: item.label,
+							items: item.items,
+						});
+					}
+				})
+			}
+			array.push({
+				id: obj.id,
+				label: obj.label,
+				items: items
+			})
+		}
+		add(menu);
+		return array;
+	}
+
+	renderSearchResults() {
+		console.log(this.getItems(this.menu))
+		const filteredItems = this.getItems(this.menu).filter(item => {
+			return item.label.toLowerCase().startsWith(this._searchprompt.toLowerCase());
+		});
+		return html`
+			<h2>Search Resuls</h2>
+			<ul>
+				${filteredItems.length === 0 ? html` 
+					<li> No results found. </li>
+				` :
+				filteredItems.map(item => html`
+						<li>
+							<a href="${item.href}">
+								${item.label}
+							</a>
+						</li>
+					`)}
+			</ul>
+		`;
+	}
+
+	renderItem(item) {
+		if (item.hasSubmenu) {
+			return html`
+				<li>
+					<button 
+						@click=${() => this.switchTo(item.id)}
+					>
+						${item.label}
+					</button>
+				</li>
+			`;
+		}
+
+		return html`
+			<li>
+				<a href="${item.href}">
+					${item.label}
+				</a>
+			</li>
+		`;
+	}
+
+	switchTo(id: number) {
+		this._currentSubmenuId = id;
+	}
+
+
+}
